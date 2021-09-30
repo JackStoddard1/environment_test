07e8ce3ef36a90d5bc3bf0a329696fc61d1b7422
root <root@localhost>
2021-09-30 09:11:31 +0000

    added Procfile and Route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
       get :delete
     end
   end
+
+  root 'books#index'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
