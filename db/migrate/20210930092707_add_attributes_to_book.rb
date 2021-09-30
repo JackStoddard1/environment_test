6fb51c7465bcb1a7fa101357afe22273e9608b06
root <root@localhost>
2021-09-30 09:44:25 +0000

    Deleted 2nd failing migration file

--- db/migrate/20210930092707_add_attributes_to_book.rb
+++
@@ -1,8 +0,0 @@
-class AddAttributesToBook < ActiveRecord::Migration[6.1]
-  def change
-    add_column :books, :title, :string
-    add_column :books, :author, :string
-    add_column :books, :price, :numeric
-    add_column :books, :publishedDate, :date
-  end
-end
